f4083c7b31fdff0150b6d246d42b285e28d0519d
Aditya Tile <adityatile99@gmail.com>
2025-07-02 19:43:43 +0530

    create car class and write properties and methods

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,44 @@ public class Main {
 
     }
 }
+
+class Car {
+    private String carId;
+    private String brand;
+    private String model;
+    private double basePricePerDay;
+    private boolean isAvailale;
+
+    public Car(String carId, String brand, String model, double basePricePerDay) {
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isAvailale = true;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public double calculatePrice(int rentalDays) {
+        return basePricePerDay * rentalDays;
+    }
+    public boolean isAvailale() {
+        return isAvailale;
+    }
+    public void rent() {
+        isAvailale = false;
+    }
+    public void returnCar() {
+        isAvailale = true;
+    }
+
+
+
+}
